a5aad92b573c01cd6e117e350fb863db1d03adc0
Felix Kok <felix.kcy@gmail.com>
2018-11-03 17:01:21 +0000

    510

--- pricer.py
+++ pricer.py
@@ -3,7 +3,7 @@ from scipy.special import comb
 
 # Default number of steps to use.
 # Feel free to change this value and see how it affects your score.
-N_STEPS = 600
+N_STEPS = 510
 
 
 def pricer(spot, strike, tau, rate, vola, steps=N_STEPS):
