9d4fa29b9ff5b83ebde5d16c9090db40e4075b03
Felix Kok <felix.kcy@gmail.com>
2018-11-03 15:16:39 +0000

    first try

--- pricer.py
+++ pricer.py
@@ -19,6 +19,20 @@ def pricer(spot, strike, tau, rate, vola, steps=N_STEPS):
     :rtype: float
     """
 
-    return 0
+    delta_t = tau / steps
+    u = np.exp(vola*np.sqrt(delta_t))
+    d = 1 / u
+    p = (np.exp(rate*delta_t) - d) / (u - d)
+    disc_factor = np.exp(-rate*delta_t)
+    
+    returns = [spot * u ** (steps - i) * d ** i for i in reversed(range(steps+1))]
+    backward = np.array(returns) - strike
+    backward[backward < 0] = 0
+    
+    p_star = (1 - d) / (u - d)
+    for n in reversed(range(N)):
+        backward = [backward[i] * (1 - p_star) + backward[i+1] * p_star for i range(n+1)]
+    
+    return backward[0]
 
 
